6220d58829647a9a2d4370e99bcaa9d21d56620b
Prakash <prakaships@outlook.com>
2026-05-30 16:10:50 +0100

    Backend initial setup

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const app = express();
+
+// MiddleWare
+// Set Security HTTP Middleware
+app.use(helmet()); // It'll produce middleware function and add it to here
+
+app.use(morgan('dev'));
+
+// Setting static file path
+app.use(express.static(`${__dirname}/public`));
+
+// Testing middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// Handle CORS error
+app.use(cors());
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+
+app.get('/', (req, res) => {
+  res.send('Server running');
+});
+
+// Handle all the unhandled Routes
+app.all('/{*any}', (req, res, next) => {
+  next(
+      new AppError(`Unable to find the ${req.originalUrl} in the server!!!`, 404),
+  );
+});
+
+// app.use(genericErrorHandler);
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+const mongoose = require('mongoose');
+
+process.on('uncaughtException', (err) => {
+    console.log('Uncaught Exception...❌❌❌');
+    const error = Object.create(err);
+    console.log(error.name, error.message);
+    process.exit(1);
+});
+
+const app = require('./app');
+
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD,
+);
+
+// const DB = process.env.DATABASE_LOCAL;
+console.log('🔍 DB string:', DB);
+
+mongoose
+    .connect(DB, {
+        serverSelectionTimeoutMS: 10000,
+        socketTimeoutMS: 45000,
+    })
+    .then(() => {
+        console.log('✅ DB Connected Successfully');
+    })
+    .catch((err) => {
+        console.log('❌ DB Connection Failed:', err.message);
+        process.exit(1);
+    });
+
+// console.log(app.get('env'));
+// console.log(process.env);
+
+// Start the Server
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => {
+    console.log(`App Running in post ${PORT}`);
+});
+
+
+process.on('unhandledRejection', (err) => {
+    console.log('UnHandled Rejection...❌❌❌');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
